167a04c922443780ce2682504940d28af114abd4
LuisOlvera <luis.olvera@tralix.com>
2015-02-09 16:34:26 -0600

    Se agrego metodos de test y allZero_Score0 withoutStrikeandSpare_Score30 y se modificaron metodos score y roll para que realizaran suma de pins derribados

--- src/clases/Game.java
+++ src/clases/Game.java
@@ -5,14 +5,20 @@ package clases;
  */
 public class Game {
     int score=0;
+    int[] rolls = new int[21];
+    int roll =0;
 
    public void roll(int pins){
-
+    rolls[roll++]=pins;
 
    }
 
     public int score (){
-
+        int indexRoll=0;
+        for(int indexFrame=0;indexFrame<10; indexFrame++)
+        {
+            score += rolls[indexRoll];
+        }
     return score;
     }
 
--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,11 +1,59 @@
 package test;
 
+import clases.Game;
+import org.junit.Assert;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
 
+    int finalScore=0;
     @org.junit.Test
-    public void allZero() throws Exception {
+    public void allZero_Score0() throws Exception {
+        Game gameBowling= new Game();
+        for (int indexRoll=0;indexRoll<20; indexRoll++)
+        {
+            gameBowling.roll(0);
+        }
+
+        finalScore=gameBowling.score();
+        Assert.assertEquals(finalScore, 0);
+
+    }
+    public void withoutStrikeandSpare_Score30() throws Exception {
+        Game gameBowling= new Game();
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        gameBowling.roll(2);
+        gameBowling.roll(1);
+
+        finalScore=gameBowling.score();
+        Assert.assertEquals(finalScore, 30);
 
     }
 }
