beb3bab9cd395b2c68f369ce0cf277f2ef4c575b
LuisOlvera <luis.olvera@tralix.com>
2015-02-09 16:19:30 -0600

    Initial commit

---
+++ src/clases/Game.java
@@ -0,0 +1,19 @@
+package clases;
+
+/**
+ * Created by root on 9/02/15.
+ */
+public class Game {
+    int score=0;
+
+   public void roll(int pins){
+
+
+   }
+
+    public int score (){
+
+    return score;
+    }
+
+}
---
+++ src/test/GameTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @org.junit.Test
+    public void allZero() throws Exception {
+
+    }
+}
